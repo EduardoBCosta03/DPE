f40d02822b4536bac01884ad4bf57d2e37d5af48
EduardoBCosta03 <eduardo.prisma03@gmail.com>
2026-03-05 21:31:06 -0300

    feat: integrate DatabaseModule into ProductsModule and update create method return type in ProductsService

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, ConfigModule],
   providers: [PrismaService],
   exports: [PrismaService],
 })
--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -1,14 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/clients.dto';
-import { BatchPayload } from 'generated/prisma/internal/prismaNamespace';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
-  async create(@Body() productDto: CreateProductDto[]): Promise<BatchPayload> {
+  async create(@Body() productDto: CreateProductDto[]) {
     return await this.productsService.create(productDto);
   }
 }
