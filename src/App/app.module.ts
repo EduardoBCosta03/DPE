c9252d6b48aac2e2eea33820bd9b5add2360cb45
EduardoBCosta03 <eduardo.prisma03@gmail.com>
2026-03-05 21:13:54 -0300

    feat: add DatabaseModule and PrismaService for database integration, update ESLint config and package dependencies

--- src/App/app.module.ts
+++ src/App/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from 'src/modules/products/products.module';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
